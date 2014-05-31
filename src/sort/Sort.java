7bc326f72ba610546cd44151094dda9fa5582db4
Brian Burns <bburns.km@gmail.com>
2014-05-31 03:21:06 -0500

    working on counting sort. test passes. next handle min.

--- src/sort/Sort.java
+++ src/sort/Sort.java
@@ -7,25 +7,65 @@ package sort;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Collections;
+
+import static java.util.Arrays.asList;
+
 
 
 public class Sort {
 
-    // bubble sort
-    // returns new list (with same object references)
-    public static <T extends Comparable> List<T> bubble(List<T> a) {
-        List<T> b = new ArrayList<>(a); // shallow copy
-        int n = b.size();
+    // Bubble sort
+    // Returns new list (with same object references)
+    public static <T extends Comparable> List<T> bubble(List<T> in) {
+        List<T> a = new ArrayList<>(in); // shallow copy
+        int n = a.size();
         for (int i = 1; i < n; i++) {
             for (int j = 0; j < n-i; j++) {
-                if (b.get(j).compareTo(b.get(j+1)) > 0) {
-                    T t = b.get(j);
-                    b.set(j, b.get(j+1));
-                    b.set(j+1, t);
+                if (a.get(j).compareTo(a.get(j+1)) > 0) {
+                    T t = a.get(j);
+                    a.set(j, a.get(j+1));
+                    a.set(j+1, t);
                 }
             }
         }
-        return b;
+        return a;
     }
+
+
+
+    // Counting sort
+    public static List<Integer> counting(List<Integer> in) {
+
+        List<Integer> out = new ArrayList<>();
+
+        // get min/max values
+        int min = Collections.min(in);
+        int max = Collections.max(in);
+//        int min = Integer.MAX_VALUE;
+//        int max = Integer.MIN_VALUE;
+//        for (Integer v : in) { }
+
+
+        // tally observed numbers
+        int nplaces = max + 1;
+        int[] counts = new int[nplaces];
+        for (Integer v : in) {
+            counts[v] += 1;
+        }
+
+        // walk through tally array, outputting numbers
+        int i = 0;
+        while (i < nplaces) {
+            while (counts[i] > 0) {
+                out.add(i);
+                counts[i]--;
+            }
+            i++;
+        }
+
+        return out;
+    }
+
 
 }
--- test/sort/SortTest.java
+++ test/sort/SortTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 
+import java.util.Collections;
 import java.util.List;
 import java.util.ArrayList;
 import static java.util.Arrays.asList;
@@ -19,30 +20,51 @@ import static java.util.Arrays.asList;
 public class SortTest {
 
     // test if a list is sorted
-    public <T extends Comparable<? super T>> boolean isSorted(List<T> list) {
-        for (int i=0; i < list.size()-1; i++) {
-            if (list.get(i).compareTo(list.get(i+1)) > 0) return false; // list[i] > list[i+1]
-        }
-        return true;
-    }
+//    public <T extends Comparable<? super T>> boolean isSorted(List<T> list) {
+//        for (int i=0; i < list.size()-1; i++) {
+//            if (list.get(i).compareTo(list.get(i+1)) > 0) return false; // list[i] > list[i+1]
+//        }
+//        return true;
+//    }
 
 
-    @Before public void setUp() throws Exception {}
+//    @Before public void setUp() throws Exception {}
 
 
     @Test
     public void testBubbleInts() throws Exception {
-        List<Integer> in = asList(2, 1, 5, 3, 0);
+        List<Integer> in = asList(2, 1, 5, 3, 0, 5);
         List<Integer> out = Sort.bubble(in);
-        assertTrue(isSorted(out));
+//        assertEquals(asList(0,1,2,3,5,5), out);
+        Collections.sort(in);
+        assertEquals(in, out);
     }
 
     @Test
     public void testBubbleStrings() throws Exception {
-        List<String> in = asList("marsh", "lake", "river", "pond", "creek");
+        List<String> in = asList("marsh", "lake", "river", "pond", "creek", "lake");
         List<String> out = Sort.bubble(in);
-        assertTrue(isSorted(out));
+//        assertTrue(isSorted(out));
+//        assertString("[creek, lake, marsh, pond, river]", out);
+        Collections.sort(in);
+        assertEquals(in, out);
     }
+
+
+    @Test
+    public void testCounting() throws Exception {
+        List<Integer> in = asList(2, 1, 5, 3, 0, 2, 0);
+        System.out.println(in);
+        List<Integer> out = Sort.counting(in);
+        System.out.println(out);
+//        List<Integer> sorted = asList(0,1,2,3,5);
+//        assertEquals(sorted, out);
+//        assertEquals(asList(0,1,2,3,5), out);
+//        assertEquals(asList(0,0,1,2,2,3,5), out);
+        Collections.sort(in);
+        assertEquals(in, out);
+    }
+
 
 
 }
