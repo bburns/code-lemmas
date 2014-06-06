9d63864278e011b4791ecf936ed3e2fa2acb36f3
Brian Burns <bburns.km@gmail.com>
2014-06-06 15:09:22 -0500

    updating readme, dfs and bfs comments

--- README.md
+++ README.md
@@ -1,16 +1,23 @@
 
+Lemmas  
+======
 
-Implementation of a few data structures and algorithms. 
+Implementating some data structures and algorithms in Java. In a way, these are like lemmas in mathematics - a step up from axioms (which would correspond to built in data types and operations), and which are used to construct larger theorems. 
 
-Trie
-Hash
-Graph
-Tree
+These are implemented better elsewhere, of course. 
 
-Depth First Search
-Breadth First Search
-Dijkstra's Algorithm
-Radix Sort
+* Data Structures
+    * Graph
+    * Trie
+* Sort
+    * Bubble Sort
+    * Counting Sort
+* Search
+    * Depth First Search
+    * Breadth First Search
 
 
-Using Ant, JUnit, Java Collections. 
+**lem&#183;ma** (plural lem&#183;mas or lem&#183;ma&#183;ta), noun. a proposition proved or accepted for immediate use in the proof of some other proposition; a helping theorem. 1560-70;  < Latin:  theme, title, epigram < Ancient Greek &#955;&#951;&#956;&#956;&#945;, something received, premise. [1]
+
+
+[1] http://dictionary.reference.com/browse/lemma?s=t
--- src/adt/Graph.java
+++ src/adt/Graph.java
@@ -1,12 +1,9 @@
 
 // A Graph interface and some search methods
-// getEdges and getDestination are sufficient to run a depth-first-search.
 
 
 package adt;
 
-
-//import java.util.*;
 
 import java.util.Collection;
 import java.util.Set;
@@ -14,6 +11,7 @@ import java.util.HashSet;
 import java.util.Stack;
 import java.util.Queue;
 import java.util.ArrayDeque;
+
 
 
 // N for node class, E for edge class
@@ -30,7 +28,10 @@ interface Graph<N, E> {
 
     // dfs
     // Depth first search on given Graph object,
-    // starting with given node and searching for other.
+    // starting with node start and searching for node find.
+    // Returns the found node or null if not found.
+    //
+    // Uses stack to store nodes to visit
     static <N, E> N dfs(Graph<N, E> g, N start, N find) {
         Stack<N> unseen = new Stack<>();
         Set<N> seen = new HashSet<>();
@@ -38,51 +39,51 @@ interface Graph<N, E> {
         while (!unseen.empty()) {
             N n = unseen.pop();
             System.out.println("visiting " + n);
-            if (n.equals(find)) return n;
+            if (n.equals(find)) { return n; }
             seen.add(n);
-            // adding from left to right, so will be searched right to left - change if that's an issue
+            // add all unseen children from left to right, so will be searched right to left - change if that's an issue
             for (E e : g.getEdges(n)) {
                 N dest = g.getDestination(e);
-                if (!seen.contains(dest)) {
-                    unseen.push(dest);
-                }
+                if (!seen.contains(dest)) { unseen.push(dest); }
             }
         }
+        // TODO would prefer to return a NullObject, or Optional
 //        return new N("NullObject"); // can't do due to type erasure
         return null;
     }
 
 
     // bfs
-    // Breadth first search
+    // Breadth first search on given Graph object,
+    // starting with node start and searching for node find.
+    // Returns the found node or null if not found.
+    //
+    // Same algorithm as dfs, but uses queue to store nodes to visit
+    // TODO combine dfs and bfs to keep in synch
     static <N, E> N bfs(Graph<N, E> g, N start, N find) {
         Queue<N> unseen = new ArrayDeque<>();
         Set<N> seen = new HashSet<>();
-//        unseen.push(start);
         unseen.add(start);
-//        while (!unseen.empty()) {
         while (unseen.peek() != null) { // ie while unseen.hasElements
-//            N n = unseen.pop();
             N n = unseen.remove();
             System.out.println("visiting " + n);
-            if (n.equals(find)) return n;
+            if (n.equals(find)) { return n; }
             seen.add(n);
-            // adding from left to right, so will be searched right to left - change if that's an issue
+            // add all unseen children from left to right, so will be searched right to left - change if that's an issue
             for (E e : g.getEdges(n)) {
                 N dest = g.getDestination(e);
-                if (!seen.contains(dest)) {
-//                    unseen.push(dest);
-                    unseen.add(dest);
-                }
+                if (!seen.contains(dest)) { unseen.add(dest); }
             }
         }
+        // TODO would prefer to return a NullObject, or Optional
         return null;
     }
 
 
     // asString
-    // Return a string representation of the given graph -
-    // a list of nodes and where they exit to
+    // Return a string representation of the given Graph object -
+    // a list of nodes and where they exit to. eg
+    // "a->[b,c]   b->[d,a]   c->[]   d->[b]"
     static <N, E> String asString(Graph<N, E> g) {
         StringBuilder sb = new StringBuilder();
         for (N n : g.getNodes()) {
@@ -94,11 +95,7 @@ interface Graph<N, E> {
 
     // asGraphviz
     // Return a representation of the graph as a Graphviz dot file, eg
-    // digraph g {
-    //     a -> b;
-    //     a -> c;
-    //     b -> c;
-    // }
+    // "digraph g {a -> b; a -> c; b -> c;}"
     static <N, E> String asGraphviz(Graph<N, E> g) {
         StringBuilder sb = new StringBuilder();
         sb.append("digraph g {\n");
